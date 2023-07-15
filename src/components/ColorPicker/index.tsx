9f7a3a17fd0964112fa11abb99a82bcfbf499b68
Junior Dering <juniordering@hotmail.com>
2023-07-15 14:20:52 -0400

    feat: color picker

--- src/components/ColorPicker/index.tsx
+++ src/components/ColorPicker/index.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import { SketchPicker } from 'react-color';
+import { useSnapshot } from 'valtio';
+
+import state from '../../store';
 
 export function ColorPicker() {
-  return <div className="">ColorPicker</div>;
+  const snap = useSnapshot(state)
+  return <div className="absolute left-full ml-3">
+    <SketchPicker
+      color={snap.color}
+      disableAlpha
+      onChange={color => state.color = color.hex}
+    />
+  </div>;
 }
